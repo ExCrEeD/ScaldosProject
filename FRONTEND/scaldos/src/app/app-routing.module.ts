c8cf8cb04093b30df8c357c842e2ad1041932648
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2020-04-12 16:58:45 -0500

    Creacion servicio back y pagina en front

--- FRONTEND/scaldos/src/app/app-routing.module.ts
+++ FRONTEND/scaldos/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
+import {CreateGroupComponent} from "./create-group/create-group.component";
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
   { path: "login", component: LoginComponent },
-  { path: "RegisterUser", component: RegisterComponent }
+  { path: "RegisterUser", component: RegisterComponent },
+  { path: "CreateGroup", component: CreateGroupComponent}
 ];
 
 @NgModule({
