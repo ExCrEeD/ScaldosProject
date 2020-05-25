a843df7a3afbbbec1d79bc882fcca768cc10acfd
ledunedain <juvargass@poligran.edu.co>
2020-05-25 03:27:31 -0500

    arreglo de diseños

--- FRONTEND/scaldos/src/app/profile/profile.component.ts
+++ FRONTEND/scaldos/src/app/profile/profile.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  name = 'Angie Fajardo';
+  name = 'ADM';
   Lista=["HOLA SOY EL GRUPO 1","HOLA SOY EL GRUPO 6", "HOLA SOY EL GURPO 2", "HOLA SOY EL GRUPO 3", "HOLA SOY EL GRUPO 4", "HOLA SOY EL GRUPO 5"]
   role= 'docente'
   constructor() { }
